7f8c2a4becd4e456e83264da720271dcf54d66ad
cooperjm <cooper.jm@outlook.com>
2021-12-23 20:20:14 -0500

    Fixed my fix, it broke the inlining

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const inlineCSS = require('inline-css');
 
-
 app.use('/', express.static('public'));
 
 app.use(express.json());
@@ -15,21 +14,33 @@ app.post('/inline', (req,res) => {
 		preserveMediaQueries: true,
 		removeStyleTags: true,
 	};
-  inlineCSS(html, options).then((html) => {    
-    return (html = replaceText(html));            
-  }).then(html => {
-    let emails = {
-      greenville: html,
-      anderson: linkChange(html,'anderson'),
-      spartanburg: linkChange(html,'spartanburg')
-    };
-    return emails;
-  }).then(emails => {
-    res.status(200).json(emails);
-  }).catch((err) => {
-    console.log(err)
-  });
+
+  doTheStuff(html, options, res);  
+  
 });
+
+async function doTheStuff(html, options, res) {
+  let code;
+  let emails;
+  try {
+    code = await quoteChange(html);
+		code = await inlineCSS(code, options);
+		code = await replaceText(code);
+    emails = splitEmails(code);
+  } catch (error) {
+    res.status(500).json(error);
+  }    
+  res.status(200).json(emails);
+}
+
+function splitEmails(html) {
+  let e = {
+		greenville: html,
+		anderson: linkChange(html, 'anderson'),
+		spartanburg: linkChange(html, 'spartanburg'),
+	};
+	return e;
+}
 
 function replaceText(html) {
   return html = html
@@ -42,6 +53,13 @@ function replaceText(html) {
 function linkChange(html, link) {
   let email = html.split('greenville').join(link);
   return email;
+}
+
+function quoteChange(code) {
+  return new Promise((resolve, reject) => {
+    resolve(code.split('&quot;').join("'"));
+    reject('oops something went wrong!');
+  });
 }
 
 app.listen(port, () => {
